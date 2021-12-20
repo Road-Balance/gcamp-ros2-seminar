6807bb1b4ef7c420d2f3954c2ff15dfe211cbb1a
kimsooyoung <tge1375@naver.com>
2021-12-21 00:47:43 +0900

    Feature : slam demo added

--- MaRo/maro_demo/launch/slam_demo.launch.py
+++ MaRo/maro_demo/launch/slam_demo.launch.py
@@ -4,6 +4,9 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import TimerAction, IncludeLaunchDescription
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
 
@@ -15,16 +18,43 @@ def generate_launch_description():
         output='screen'
     )
 
+    # rf2o node
+    rf2o_node = Node(
+        package='rf2o_laser_odometry',
+        node_executable='rf2o_laser_odometry_node',
+        node_name='rf2o_laser_odometry',
+        output='screen',
+        parameters=[
+            {
+                'laser_scan_topic' : '/scan',
+                'odom_topic' : '/odom_rf2o',
+                'publish_tf' : True,
+                'base_frame_id' : 'base_footprint',
+                'odom_frame_id' : 'odom',
+                'init_pose_from_topic' : '',
+                'freq' : 10.0
+            }
+        ],
+    )
+
     # Tf publish
     static_transform_publisher = Node(
         package = "tf2_ros", 
         node_executable = "static_transform_publisher",
-        arguments = "0 0 0 0 0 0 world laser_frame".split()
+        arguments = "0 0 0 0 0 0 base_footprint laser_frame".split()
+    )
+
+    # rplidar node
+    slam_pkg_path = os.path.join(get_package_share_directory('slam_toolbox'))
+    slam_launch_path = os.path.join(slam_pkg_path, 'launch', 'online_async_launch.py')
+
+    slam_toolbox_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(slam_launch_path)
     )
 
     # Rviz with .rviz config
-    pkg_path = os.path.join(get_package_share_directory('maro_demo'))
-    rviz_config_file = os.path.join(pkg_path, 'rviz', 'gazebo.rviz')
+    demo_pkg_path = os.path.join(get_package_share_directory('maro_demo'))
+    rviz_config_file = os.path.join(demo_pkg_path, 'rviz', 'slam_demo.rviz')
 
     rviz = Node(
         package='rviz2',
@@ -35,7 +65,12 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        TimerAction(
+            period=5.0,
+            actions=[rviz]
+        ),
         rplidar_node,
+        rf2o_node,
         static_transform_publisher,
-        # rviz,
+        slam_toolbox_node,
     ])
